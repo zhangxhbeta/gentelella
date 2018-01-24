7f19e56f00811342570ef9db65a4c1d1b45d7a37
funilrys <contact@funilrys.com>
2018-01-24 09:56:54 +0100

    Introduction of validator.js

--- build/js/custom.js
+++ build/js/custom.js
@@ -4982,6 +4982,42 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    if (typeof(validator) === 'undefined') {
+        return;
+    }
+    console.log('init_validator');
+
+    // initialize the validator function
+    validator.message.date = 'not a real date';
+
+    // validate a field on "blur" event, a 'select' on 'change' event & a '.reuired' classed multifield on 'keyup':
+    $('form')
+        .on('blur', 'input[required], input.optional, select.required', validator.checkField)
+        .on('change', 'select.required', validator.checkField)
+        .on('keypress', 'input[required][pattern]', validator.keypress);
+
+    $('.multi.required').on('keyup blur', 'input', function() {
+        validator.checkField.apply($(this).siblings().last()[0]);
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        var submit = true;
+
+        // evaluate the form using generic validaing
+        if (!validator.checkAll($(this))) {
+            submit = false;
+        }
+
+        if (submit)
+            this.submit();
+
+        return false;
+    });
+
+});
+
+$(document).ready(function() {
     if (typeof($.fn.wysiwyg) === 'undefined') {
         return;
     }
