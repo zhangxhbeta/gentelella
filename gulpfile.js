3495fa1251c69580795a8ea49f3a0677bccc7620
funilrys <contact@funilrys.com>
2018-01-24 09:37:10 +0100

    Review of gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,40 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
-var DEST = 'build/';
+var DEST = 'build/',
+    PROJECT_FILE_NAME = 'custom';
 
 gulp.task('scripts', function() {
     return gulp.src([
-        'src/js/helpers/*.js',
-        'src/js/*.js',
-      ])
-      .pipe(concat('custom.js'))
-      .pipe(gulp.dest(DEST+'/js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest(DEST+'/js'))
-      .pipe(browserSync.stream());
+            'src/js/helpers/*.js',
+            'src/js/*.js',
+        ])
+        .pipe(concat(PROJECT_FILE_NAME + '.js'))
+        .pipe(gulp.dest(DEST + '/js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(DEST + '/js'))
+        .pipe(browserSync.stream());
 });
 
-// TODO: Maybe we can simplify how sass compile the minify and unminify version
-var compileSASS = function (filename, options) {
-  return sass('src/scss/app.scss', options)
+var compileSASS = function(filename, options) {
+    return sass('src/scss/app.scss', options)
         .pipe(autoprefixer('last 2 versions', '> 5%'))
         .pipe(concat(filename))
-        .pipe(gulp.dest(DEST+'/css'))
+        .pipe(gulp.dest(DEST + '/css'))
         .pipe(browserSync.stream());
 };
 
 gulp.task('sass', function() {
-    return compileSASS('custom.css', {});
+    return compileSASS(PROJECT_FILE_NAME + '.css', {});
 });
 
 gulp.task('sass-minify', function() {
-    return compileSASS('custom.min.css', {style: 'compressed'});
+    return compileSASS(PROJECT_FILE_NAME + '.min.css', {
+        style: 'compressed'
+    });
 });
 
 gulp.task('browser-sync', function() {
@@ -48,12 +52,12 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  // Watch .html files
-  gulp.watch('production/*.html', browserSync.reload);
-  // Watch .js files
-  gulp.watch('src/js/*.js', ['scripts']);
-  // Watch .scss files
-  gulp.watch(['src/scss/**/*.scss','src/scss/*.scss'], ['sass', 'sass-minify']);
+    // Watch .html files
+    gulp.watch('production/*.html', browserSync.reload);
+    // Watch .js files
+    gulp.watch('src/js/*.js', ['scripts']);
+    // Watch .scss files
+    gulp.watch(['src/scss/**/*.scss', 'src/scss/*.scss'], ['sass', 'sass-minify']);
 });
 
 // Default Task
