5c75d569617a21c3f77aff916636e676e9c81789
funilrys <contact@funilrys.com>
2018-01-24 09:28:19 +0100

    Introduction of accordion.js

---
+++ src/js/accordion.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".expand").on("click", function () {
+        $(this).next().slideToggle(200);
+        $expand = $(this).find(">:first-child");
+
+        if ($expand.text() == "+") {
+            $expand.text("-");
+        } else {
+            $expand.text("+");
+        }
+    });
+});
