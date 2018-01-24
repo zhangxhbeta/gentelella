124d02c171f5b3a8d97d939f08ae0ae64ae67e1e
funilrys <contact@funilrys.com>
2018-01-24 09:25:50 +0100

    Introduction of _base.js
    Please note that this file will save all global variables or functions

---
+++ src/js/_base.js
@@ -0,0 +1,13 @@
+var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
+    $BODY = $('body'),
+    $MENU_TOGGLE = $('#menu_toggle'),
+    $SIDEBAR_MENU = $('#sidebar-menu'),
+    $SIDEBAR_FOOTER = $('.sidebar-footer'),
+    $LEFT_COL = $('.left_col'),
+    $RIGHT_COL = $('.right_col'),
+    $NAV_MENU = $('.nav_menu'),
+    $FOOTER = $('footer');
+
+var randNum = function() {
+    return (Math.floor(Math.random() * (1 + 40 - 20))) + 20;
+};
