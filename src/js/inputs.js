2595c580f87cac3b36b23d7d83cc0c822faa6d06
funilrys <contact@funilrys.com>
2018-01-24 09:31:12 +0100

    Introductino of inputs.js

---
+++ src/js/inputs.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    function onAddTag(tag) {
+        alert("Added a tag: " + tag);
+    }
+
+    function onRemoveTag(tag) {
+        alert("Removed a tag: " + tag);
+    }
+
+    function onChangeTag(input, tag) {
+        alert("Changed a tag: " + tag);
+    }
+});
