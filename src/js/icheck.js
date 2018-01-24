4e26964ebc39e4beee357987607affa0c13e4879
funilrys <contact@funilrys.com>
2018-01-24 09:30:38 +0100

    Introductino of icheck.js

---
+++ src/js/icheck.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    if ($("input.flat")[0]) {
+        $(document).ready(function () {
+            $('input.flat').iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass: 'iradio_flat-green'
+            });
+        });
+    }
+});
