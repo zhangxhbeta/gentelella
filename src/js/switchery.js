d460dfdc3107f7756f002c6a0ac356ec3c4b392a
funilrys <contact@funilrys.com>
2018-01-24 09:34:17 +0100

    Introductino of switchery.js

---
+++ src/js/switchery.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    if ($(".js-switch")[0]) {
+        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+        elems.forEach(function (html) {
+            var switchery = new Switchery(html, {
+                color: '#26B99A'
+            });
+        });
+    }
+});
