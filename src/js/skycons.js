81a3e55ce0d15c554865fe91a13a44c5081ae38e
funilrys <contact@funilrys.com>
2018-01-24 09:33:27 +0100

    Introduction of skycons.js

---
+++ src/js/skycons.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    if (typeof(Skycons) === 'undefined') {
+        return;
+    }
+    console.log('init_skycons');
+
+    var icons = new Skycons({
+            "color": "#73879C"
+        }),
+        list = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+        ],
+        i;
+
+    for (i = list.length; i--;)
+        icons.set(list[i], list[i]);
+
+    icons.play();
+});
