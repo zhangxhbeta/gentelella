c72cffe12bff7f7575b4c4592cfc581b5d01dc5a
funilrys <contact@funilrys.com>
2018-01-24 09:32:56 +0100

    Introduction of select2.js

---
+++ src/js/select2.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    if (typeof(select2) === 'undefined') {
+        return;
+    }
+    console.log('init_toolbox');
+
+    $(".select2_single").select2({
+        placeholder: "Select a state",
+        allowClear: true
+    });
+    $(".select2_group").select2({});
+    $(".select2_multiple").select2({
+        maximumSelectionLength: 4,
+        placeholder: "With Max Selection limit 4",
+        allowClear: true
+    });
+});
