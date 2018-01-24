65d1b9a5ad8a49a8e008182f76c35acf8d2e02e7
funilrys <contact@funilrys.com>
2018-01-24 09:31:38 +0100

    Introduction of nprogress.js

---
+++ src/js/nprogress.js
@@ -0,0 +1,9 @@
+if (typeof NProgress != 'undefined') {
+    $(document).ready(function () {
+        NProgress.start();
+    });
+
+    $(window).on('load', function() {
+        NProgress.done();
+    });
+}
